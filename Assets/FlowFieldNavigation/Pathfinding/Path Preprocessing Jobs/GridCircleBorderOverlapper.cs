a7aabcfd709077db113c5f1728815e1b0cf88480
Morterolath <ayberkk0705@gmail.com>
2024-06-30 22:15:29 +0300

    grid circle overlap now works perfectly

--- Assets/FlowFieldNavigation/Pathfinding/Path Preprocessing Jobs/GridCircleBorderOverlapper.cs
+++ Assets/FlowFieldNavigation/Pathfinding/Path Preprocessing Jobs/GridCircleBorderOverlapper.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Unity.Collections;
 using Unity.Mathematics;
+using UnityEditor.SceneManagement;
 
 namespace FlowFieldNavigation
 {
@@ -22,48 +23,57 @@ namespace FlowFieldNavigation
         const int AREA_BOTLEFT = SMALLER_THAN_MAX_Y | SMALLER_THAN_MAX_X;
         const int AREA_BOTMID = BUGGER_THAN_MIN_X | SMALLER_THAN_MAX_Y | SMALLER_THAN_MAX_X;
         const int AREA_BOTRIGHT = BUGGER_THAN_MIN_X | SMALLER_THAN_MAX_Y;
-        const byte STAG_TOPRIGHT = 1;
+        const byte STAGE_FINISHED = 0;
+        const byte STAGE_TOPRIGHT = 1;
         const byte STAGE_BOTRIGHT = 2;
         const byte STAGE_BOTLEFT = 3;
         const byte STAGE_TOPLEFT = 4;
-        internal float2 GoalPosition;
-        internal float GoalRange;
-
-        internal NativeArray<int> BorderSectors()
+        float2 _circleCenter;
+        float _circleRadius;
+        int2 _startIndex;
+        int2 _previousIndex;
+        int2 _currentIndex;
+        byte _lookupStage;
+        internal GridCircleBorderOverlapper(float2 circleCenter, float circleRadius)
         {
-            NativeList<int> borderSectors = new NativeList<int>(Allocator.Temp);
-            float2 topPosition = GoalPosition + new float2(0, GoalRange);
-            int2 topIndex = FlowFieldUtilities.PosTo2D(topPosition, FlowFieldUtilities.TileSize, FlowFieldUtilities.FieldGridStartPosition);
-            int2 topSector2d = FlowFieldUtilities.GetSector2D(topIndex, FlowFieldUtilities.SectorColAmount);
-
-            int2 curSector2d = topSector2d;
-            byte stage = STAG_TOPRIGHT;
-            do
+            _circleCenter = circleCenter;
+            _circleRadius = circleRadius;
+            _currentIndex = 0;
+            _previousIndex = 0;
+            _startIndex = 0;
+            _lookupStage = STAGE_FINISHED;
+        }
+        internal void Start()
+        {
+            float2 topPosition = _circleCenter + new float2(0, _circleRadius);
+            int2 topIndex = FlowFieldUtilities.PosTo2D(topPosition, FlowFieldUtilities.TileSize * FlowFieldUtilities.SectorColAmount, FlowFieldUtilities.FieldGridStartPosition);
+            _currentIndex = topIndex;
+            _startIndex = topIndex;
+            _lookupStage = STAGE_TOPRIGHT;
+        }
+        internal bool TryGetCurrent(out int2 currentIndex)
+        {
+            currentIndex = _currentIndex;
+            return _lookupStage != STAGE_FINISHED;
+        }
+        internal void MoveNext()
+        {
+            //Is finished
+            if (_lookupStage == STAGE_FINISHED)
             {
-                bool nOverflow = curSector2d.y >= FlowFieldUtilities.SectorMatrixRowAmount;
-                bool eOverflow = curSector2d.x >= FlowFieldUtilities.SectorMatrixColAmount;
-                bool sOverflow = curSector2d.y < 0;
-                bool wOverflow = curSector2d.x < 0;
-                if (!(nOverflow || eOverflow || sOverflow || wOverflow))
-                {
-                    borderSectors.Add(FlowFieldUtilities.To1D(curSector2d, FlowFieldUtilities.SectorMatrixColAmount));
-                }
-
-                int2 pickedSector = curSector2d;
-                bool succesfull = false;
-                while (!succesfull)
-                {
-                    succesfull = RunWithStage(curSector2d, stage, out pickedSector);
-                    if (!succesfull)
-                    {
-                        stage++;
-                    }
-                }
-                curSector2d = pickedSector;
-
+                return;
             }
-            while (!curSector2d.Equals(topSector2d));
-            return borderSectors.AsArray();
+
+            int2 newIndex = _currentIndex;
+            bool succesfull = false;
+            while (!succesfull)
+            {
+                succesfull = RunWithStage(_currentIndex, _lookupStage, out newIndex);
+                _lookupStage = (byte) math.select((_lookupStage + 1), _lookupStage, succesfull);
+            }
+            _previousIndex = _currentIndex;
+            _currentIndex = newIndex;
+            _lookupStage = (byte) math.select((int) _lookupStage, STAGE_FINISHED, _currentIndex.Equals(_startIndex));
         }
         bool RunWithStage(int2 curSector, byte stage, out int2 pickedSector)
         {
@@ -74,7 +84,7 @@ namespace FlowFieldNavigation
 
             switch (stage)
             {
-                case STAG_TOPRIGHT:
+                case STAGE_TOPRIGHT:
                     n1 += new int2(1, 0);
                     n2 += new int2(0, -1);
                     n3 += new int2(1, -1);
@@ -94,18 +104,21 @@ namespace FlowFieldNavigation
                     n2 += new int2(1, 0);
                     n3 += new int2(1, 1);
                     break;
+                default:
+                    pickedSector = _startIndex;
+                    return true;
             }
-            if (IsBorderSector(n1))
+            if (IsBorderSector(n1) && !n1.Equals(_previousIndex))
             {
                 pickedSector = n1;
                 return true;
             }
-            if (IsBorderSector(n2))
+            if (IsBorderSector(n2) && !n2.Equals(_previousIndex))
             {
                 pickedSector = n2;
                 return true;
             }
-            if (IsBorderSector(n3))
+            if (IsBorderSector(n3) && !n3.Equals(_previousIndex))
             {
                 pickedSector = n3;
                 return true;
@@ -117,8 +130,8 @@ namespace FlowFieldNavigation
             float sectorSize = FlowFieldUtilities.SectorColAmount * FlowFieldUtilities.TileSize;
             float2 sectorMin = sector2d * math.float2(FlowFieldUtilities.TileSize) * FlowFieldUtilities.SectorColAmount + FlowFieldUtilities.FieldGridStartPosition;
             float2 sectorMax = sectorMin + sectorSize;
-            bool2 goalSmallerThanSectorMaxPos = GoalPosition <= sectorMax;
-            bool2 goalBiggerThanSectorMinPos = GoalPosition >= sectorMin;
+            bool2 goalSmallerThanSectorMaxPos = _circleCenter <= sectorMax;
+            bool2 goalBiggerThanSectorMinPos = _circleCenter >= sectorMin;
             int resultBits = math.select(0, SMALLER_THAN_MAX_Y, goalSmallerThanSectorMaxPos.y);
             resultBits |= math.select(0, BIGGER_THAN_MIN_Y, goalBiggerThanSectorMinPos.y);
             resultBits |= math.select(0, SMALLER_THAN_MAX_X, goalSmallerThanSectorMaxPos.x);
@@ -133,21 +146,21 @@ namespace FlowFieldNavigation
             switch (resultBits)
             {
                 case AREA_TOPLEFT:
-                    return math.distance(sectorTopLeft, GoalPosition) <= GoalRange && math.distance(sectorBotRight, GoalPosition) > GoalRange;
+                    return math.distance(sectorTopLeft, _circleCenter) <= _circleRadius && math.distance(sectorBotRight, _circleCenter) > _circleRadius;
                 case AREA_TOPMID:
-                    return (GoalPosition.y - sectorMax.y) <= GoalRange && math.max(math.distance(sectorBotRight, GoalPosition), math.distance(sectorBotLeft, GoalPosition)) > GoalRange;
+                    return (_circleCenter.y - sectorMax.y) <= _circleRadius && math.max(math.distance(sectorBotRight, _circleCenter), math.distance(sectorBotLeft, _circleCenter)) > _circleRadius;
                 case AREA_TOPRIGHT:
-                    return math.distance(sectorTopRight, GoalPosition) <= GoalRange && math.distance(sectorBotLeft, GoalPosition) > GoalRange;
+                    return math.distance(sectorTopRight, _circleCenter) <= _circleRadius && math.distance(sectorBotLeft, _circleCenter) > _circleRadius;
                 case AREA_MIDLEFT:
-                    return (sectorMin.x - GoalPosition.x) <= GoalRange && math.max(math.distance(sectorTopRight, GoalPosition), math.distance(sectorBotRight, GoalPosition)) > GoalRange;
+                    return (sectorMin.x - _circleCenter.x) <= _circleRadius && math.max(math.distance(sectorTopRight, _circleCenter), math.distance(sectorBotRight, _circleCenter)) > _circleRadius;
                 case AREA_MIDRIGHT:
-                    return (GoalPosition.x - sectorMax.x) <= GoalRange && math.max(math.distance(sectorTopLeft, GoalPosition), math.distance(sectorBotLeft, GoalPosition)) > GoalRange;
+                    return (_circleCenter.x - sectorMax.x) <= _circleRadius && math.max(math.distance(sectorTopLeft, _circleCenter), math.distance(sectorBotLeft, _circleCenter)) > _circleRadius;
                 case AREA_BOTLEFT:
-                    return math.distance(sectorBotLeft, GoalPosition) <= GoalRange && math.distance(sectorTopRight, GoalPosition) > GoalRange;
+                    return math.distance(sectorBotLeft, _circleCenter) <= _circleRadius && math.distance(sectorTopRight, _circleCenter) > _circleRadius;
                 case AREA_BOTMID:
-                    return (sectorMin.y - GoalPosition.y) <= GoalRange && math.max(math.distance(sectorTopLeft, GoalPosition), math.distance(sectorTopRight, GoalPosition)) > GoalRange;
+                    return (sectorMin.y - _circleCenter.y) <= _circleRadius && math.max(math.distance(sectorTopLeft, _circleCenter), math.distance(sectorTopRight, _circleCenter)) > _circleRadius;
                 case AREA_BOTRIGHT:
-                    return math.distance(sectorBotRight, GoalPosition) <= GoalRange && math.distance(sectorTopLeft, GoalPosition) > GoalRange;
+                    return math.distance(sectorBotRight, _circleCenter) <= _circleRadius && math.distance(sectorTopLeft, _circleCenter) > _circleRadius;
                 default:
                     return true;
             }
